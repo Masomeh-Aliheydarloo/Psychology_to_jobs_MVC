1bd6341049535883632168d76c58f2a9bc5357f1
Masomeh Aliheydarloo <69151573+Masomeh-Aliheydarloo@users.noreply.github.com>
2025-09-18 22:59:24 +0330

    Update main.tsx

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom'//npm i react-router-dom  npm i 
 import ReactDOM from 'react-dom/client'
 import { AuthProvider } from './components/AuthContext.tsx'
 
- import { HelmetProvider } from 'react-helmet-async'; //npm install react-helmet-async@latest
+ //import { HelmetProvider } from 'react-helmet-async'; //npm install react-helmet-async@latest
 
 
 
@@ -16,13 +16,13 @@ export const root = ReactDOM.createRoot(container as HTMLElement);
 root.render(
 
     <React.StrictMode>
-       <HelmetProvider>
+       
         <BrowserRouter>
           <AuthProvider>
             <App />
             </AuthProvider>
         </BrowserRouter>
-       </HelmetProvider>
+      
     </React.StrictMode>
 
 );
